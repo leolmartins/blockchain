334596882cdd34914e874f24038e16c89d76649b
leolmartins <leo_martins@outlook.com>
2020-11-15 19:26:47 -0300

    implementada view do historico

--- app/src/main/java/com/example/blockchain/ui/HomeContract.kt
+++ app/src/main/java/com/example/blockchain/ui/HomeContract.kt
@@ -6,21 +6,23 @@ import com.example.blockchain.shared.usecases.FetchBlockChainStatsUseCase
 /**
  * @author Leonardo Martins on 15/11/20
  */
-interface HomeContract: BaseContract {
+interface HomeContract : BaseContract {
 
-    interface View: BaseContract.View {
+    interface View : BaseContract.View {
 
         fun showLoading()
 
         fun hideLoading()
 
-        fun showToast(error: String)
+        fun showErrorToast()
+
+        fun fillMarketPrice(price: String)
     }
 
-    interface Presenter: BaseContract.Presenter {
+    interface Presenter : BaseContract.Presenter {
 
         fun fetchBlockChainStats()
     }
 
-    interface Interactor: BaseContract.Interactor, FetchBlockChainStatsUseCase
+    interface Interactor : BaseContract.Interactor, FetchBlockChainStatsUseCase
 }
